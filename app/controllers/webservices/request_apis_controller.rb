fbcd0820d9db438917ba0cef28eaf506880edc03
shashank_tripathi <shashanktripathi1993@yahoo.com>
2016-06-14 11:14:57 +0530

    change in production

--- app/controllers/webservices/request_apis_controller.rb
+++ app/controllers/webservices/request_apis_controller.rb
@@ -1,85 +1,85 @@
-class Webservices::RequestApisController < ApplicationController
-	before_filter :find_user,except: []
-	before_filter :find_friend,except: [:pending_request, :contact_list]
+# class Webservices::RequestApisController < ApplicationController
+# 	before_filter :find_user,except: []
+# 	before_filter :find_friend,except: [:pending_request, :contact_list]
 
-	def send_request
-		if @user.friend_with?(@friend)
-			render :json => {:responseCode => 500,:responseMessage => "User already present in your friend list."}
-		elsif  @user.invited_by?(@friend) or @friend.invited_by?(@user)
-			render :json => {:responseCode => 500,:responseMessage => "User already invited by you."}
-		else
-			@user.invite(@friend)
-			alert = @user.user_name + ' ' + 'sends a request to you.'
-		    activity = @friend.activities.create(activity_type: 'friend_request',item_id: @user.id,item_type: 'User', user_id: @user.id, message: alert)    		
-		    PushWorker.perform_async(@friend.id,alert,@user.id,'User',@user.user_name,activity.id)
-		    if @trend = Trending.find_by_user_id(@friend.id)
-				@trend.update_attributes(count: @trend.count+1)
-			else 
-				@trend = Trending.create(user_id: @friend.id, count: 1)
-			end	
-		  	# UserMailer.send_friend_request_mail(friend.email).deliver
-			render :json => {:responseCode => 200,:responseMessage => "You invited #{@friend.first_name} successfully."}
-		end
-	end
+# 	def send_request
+# 		if @user.friend_with?(@friend)
+# 			render :json => {:responseCode => 500,:responseMessage => "User already present in your friend list."}
+# 		elsif  @user.invited_by?(@friend) or @friend.invited_by?(@user)
+# 			render :json => {:responseCode => 500,:responseMessage => "User already invited by you."}
+# 		else
+# 			@user.invite(@friend)
+# 			alert = @user.user_name + ' ' + 'sends a request to you.'
+# 		    activity = @friend.activities.create(activity_type: 'friend_request',item_id: @user.id,item_type: 'User', user_id: @user.id, message: alert)    		
+# 		    PushWorker.perform_async(@friend.id,alert,@user.id,'User',@user.user_name,activity.id)
+# 		    if @trend = Trending.find_by_user_id(@friend.id)
+# 				@trend.update_attributes(count: @trend.count+1)
+# 			else 
+# 				@trend = Trending.create(user_id: @friend.id, count: 1)
+# 			end	
+# 		  	# UserMailer.send_friend_request_mail(friend.email).deliver
+# 			render :json => {:responseCode => 200,:responseMessage => "You invited #{@friend.first_name} successfully."}
+# 		end
+# 	end
 
-	def accept_request
-		if @friend.invited?(@user) 
-			if !@user.friend_with?(@friend)
-				@user.approve(@friend)
-				@user.friendships.find_by(friend_id: @friend.id).update_attributes(keyword: params[:user][:keyword])
-				alert = @user.user_name+' '+'accepted your request.'
-	 			activity = @friend.activities.find_or_create_by(activity_type: 'friend_request',item_id: @user.id,item_type: 'User', user_id: @user.id, message: alert)
-			    PushWorker.perform_async(@friend.id,alert,@user.id,'User',@user.user_name,activity.id)
-				render :json => {:responseCode => 200,:responseMessage => "You approved #{@friend.first_name}'s request successfully."}
-			else
-				render :json => {:responseCode => 500,:responseMessage => "#{@friend.user_name} is already in your friend list."}	
-			end
-		else
-			render :json => {:responseCode => 500,:responseMessage => "You don't have any friend request."}
-		end
-	end
+# 	def accept_request
+# 		if @friend.invited?(@user) 
+# 			if !@user.friend_with?(@friend)
+# 				@user.approve(@friend)
+# 				@user.friendships.find_by(friend_id: @friend.id).update_attributes(keyword: params[:user][:keyword])
+# 				alert = @user.user_name+' '+'accepted your request.'
+# 	 			activity = @friend.activities.find_or_create_by(activity_type: 'friend_request',item_id: @user.id,item_type: 'User', user_id: @user.id, message: alert)
+# 			    PushWorker.perform_async(@friend.id,alert,@user.id,'User',@user.user_name,activity.id)
+# 				render :json => {:responseCode => 200,:responseMessage => "You approved #{@friend.first_name}'s request successfully."}
+# 			else
+# 				render :json => {:responseCode => 500,:responseMessage => "#{@friend.user_name} is already in your friend list."}	
+# 			end
+# 		else
+# 			render :json => {:responseCode => 500,:responseMessage => "You don't have any friend request."}
+# 		end
+# 	end
 
-	def reject_request
-		if @friend
-			@user.remove_friendship(@friend)
-			alert = @user.user_name+' '+'rejected your request.'
-	 		activity = @friend.activities.find_or_create_by(activity_type: 'friend_request',item_id: @user.id,item_type: 'User', user_id: @user.id, message: alert)
-		    PushWorker.perform_async(@friend.id,alert,@user.id,'User',@user.user_name,activity.id)
-			render :json => {:responseCode => 200,:responseMessage => "You rejected #{@friend.first_name}'s request successfully."}
-		else
-			render :json => {:responseCode => 500,:responseMessage => "You don't have any friend request."}
-		end
-	end
+# 	def reject_request
+# 		if @friend
+# 			@user.remove_friendship(@friend)
+# 			alert = @user.user_name+' '+'rejected your request.'
+# 	 		activity = @friend.activities.find_or_create_by(activity_type: 'friend_request',item_id: @user.id,item_type: 'User', user_id: @user.id, message: alert)
+# 		    PushWorker.perform_async(@friend.id,alert,@user.id,'User',@user.user_name,activity.id)
+# 			render :json => {:responseCode => 200,:responseMessage => "You rejected #{@friend.first_name}'s request successfully."}
+# 		else
+# 			render :json => {:responseCode => 500,:responseMessage => "You don't have any friend request."}
+# 		end
+# 	end
 
-	def pending_request
-		render :json => {:response_code => 200,:response_message => "Pending invitations fetched successfully.",:invitaions => @user.pending_invited_by.order("created_at desc").paginate(:page => params[:page], :per_page => 10).map{|user| user.attributes.merge(:mutual_friend_count => user.common_friends_with(@user).count, :image =>  user.image.url)}}
-	end
+# 	def pending_request
+# 		render :json => {:response_code => 200,:response_message => "Pending invitations fetched successfully.",:invitaions => @user.pending_invited_by.order("created_at desc").paginate(:page => params[:page], :per_page => 10).map{|user| user.attributes.merge(:mutual_friend_count => user.common_friends_with(@user).count, :image =>  user.image.url)}}
+# 	end
 	
-	def contact_list
-		render :json => {:response_code => 200,:response_message => "Your contact(GAB) list fetched successfully.",:contacts_list => @user.friends.order("created_at desc").paginate(:page => params[:page], :per_page => 10).map{|user| user.attributes.merge(:mutual_friend_count => user.common_friends_with(@user).count, :image =>  user.image.url)}}
-	end
+# 	def contact_list
+# 		render :json => {:response_code => 200,:response_message => "Your contact(GAB) list fetched successfully.",:contacts_list => @user.friends.order("created_at desc").paginate(:page => params[:page], :per_page => 10).map{|user| user.attributes.merge(:mutual_friend_count => user.common_friends_with(@user).count, :image =>  user.image.url)}}
+# 	end
 
-	private
+# 	private
 
-	def find_user
-		if params[:user][:user_id]
-			@user = User.find_by_id(params[:user][:user_id])
-		    unless @user
-		     render :json =>  {:responseCode => 500,:responseMessage => "Oops! User not found."}
-		    end
-		else
-			render :json =>  {:responseCode => 500,:responseMessage => "Sorry! You are not an authenticated user."}
-	    end
-  	end
+# 	def find_user
+# 		if params[:user][:user_id]
+# 			@user = User.find_by_id(params[:user][:user_id])
+# 		    unless @user
+# 		     render :json =>  {:responseCode => 500,:responseMessage => "Oops! User not found."}
+# 		    end
+# 		else
+# 			render :json =>  {:responseCode => 500,:responseMessage => "Sorry! You are not an authenticated user."}
+# 	    end
+#   	end
 
-  	def find_friend
-		if params[:user][:friend_id]
-			@friend = User.find_by_id(params[:user][:friend_id])
-		    unless @friend
-		     render :json =>  {:responseCode => 500,:responseMessage => "Oops! Friend User not found."}
-		    end
-		else
-			render :json =>  {:responseCode => 500,:responseMessage => "Sorry! You are not an authenticated user."}
-	    end
-  	end
-end
+#   	def find_friend
+# 		if params[:user][:friend_id]
+# 			@friend = User.find_by_id(params[:user][:friend_id])
+# 		    unless @friend
+# 		     render :json =>  {:responseCode => 500,:responseMessage => "Oops! Friend User not found."}
+# 		    end
+# 		else
+# 			render :json =>  {:responseCode => 500,:responseMessage => "Sorry! You are not an authenticated user."}
+# 	    end
+#   	end
+# end
--- app/controllers/webservices/user_apis_controller.rb
+++ app/controllers/webservices/user_apis_controller.rb
@@ -1,128 +1,128 @@
-class Webservices::UserApisController < ApplicationController
-	before_filter :find_user,except: [:sign_up, :sign_in, :otp_confirm, :otp_resend, :social_login, :forget_password]
+# class Webservices::UserApisController < ApplicationController
+# 	before_filter :find_user,except: [:sign_up, :sign_in, :otp_confirm, :otp_resend, :social_login, :forget_password]
 
-	def sign_up
-		if @user_param = User.find_by(email: params[:user][:email].downcase)
-			render :json =>  {:responseCode => 200,:responseMessage =>"Email has already been taken."}
-		else
-	    	@otp = OtpInfo.find_or_create_by(email: params[:user][:email].downcase)
-	    	@otp.otp =  rand(1000..9999)
-	    	if @otp.save
-		    	UserMailer.send_otp(@otp).deliver_now
-			    render :json =>  {:responseCode => 200,:responseMessage =>"Your OTP successfully send to your account email, Please verify your OTP to create your account.",:user => @otp }
-   			end
-   		end
-   	end
+# 	def sign_up
+# 		if @user_param = User.find_by(email: params[:user][:email].downcase)
+# 			render :json =>  {:responseCode => 200,:responseMessage =>"Email has already been taken."}
+# 		else
+# 	    	@otp = OtpInfo.find_or_create_by(email: params[:user][:email].downcase)
+# 	    	@otp.otp =  rand(1000..9999)
+# 	    	if @otp.save
+# 		    	UserMailer.send_otp(@otp).deliver_now
+# 			    render :json =>  {:responseCode => 200,:responseMessage =>"Your OTP successfully send to your account email, Please verify your OTP to create your account.",:user => @otp }
+#    			end
+#    		end
+#    	end
 
-  	def otp_confirm
-  		@user = OtpInfo.find_by(email: params[:user][:email].downcase)
-  		if @user.otp.eql?(params[:user][:otp])
-  			@user_param = User.new(user_params)
-  			@user_param.role = "user"
-  			if @user_param.save
-	    		@device = Device.find_or_create_by(device_id: params[:device_id], device_type: params[:device_type], user_id: @user_param.id)
-	  			UserMailer.signup_confirmation(@user_param).deliver_now
-	  			render :json =>  {:responseCode => 200,:responseMessage =>"You are successfully verify your OTP and account created successfully.",:user => @user_param }
-			else
-		    	render :json =>  {:responseCode => 500,:responseMessage => @user_param.errors.full_messages.first}
-    		end
-		else
-  			render :json =>  {:responseCode => 500,:responseMessage => "Invalid OTP, Please enter a valid OTP."}
-		end	
-  	end
+#   	def otp_confirm
+#   		@user = OtpInfo.find_by(email: params[:user][:email].downcase)
+#   		if @user.otp.eql?(params[:user][:otp])
+#   			@user_param = User.new(user_params)
+#   			@user_param.role = "user"
+#   			if @user_param.save
+# 	    		@device = Device.find_or_create_by(device_id: params[:device_id], device_type: params[:device_type], user_id: @user_param.id)
+# 	  			UserMailer.signup_confirmation(@user_param).deliver_now
+# 	  			render :json =>  {:responseCode => 200,:responseMessage =>"You are successfully verify your OTP and account created successfully.",:user => @user_param }
+# 			else
+# 		    	render :json =>  {:responseCode => 500,:responseMessage => @user_param.errors.full_messages.first}
+#     		end
+# 		else
+#   			render :json =>  {:responseCode => 500,:responseMessage => "Invalid OTP, Please enter a valid OTP."}
+# 		end	
+#   	end
 
-  	def otp_resend
-  		@user = OtpInfo.find_by(email: params[:user][:email])
-  		@user.otp = rand(1000..9999)
-  		if @user.save
-  			UserMailer.send_otp(@user).deliver_now
-  			render :json =>  {:responseCode => 200,:responseMessage =>"Your OTP successfully send to your account email, Please verify your account.",:user => @user}
-		else
-  			render :json =>  {:responseCode => 500,:responseMessage => @user.errors.full_messages.first }
-		end	
-  	end
+#   	def otp_resend
+#   		@user = OtpInfo.find_by(email: params[:user][:email])
+#   		@user.otp = rand(1000..9999)
+#   		if @user.save
+#   			UserMailer.send_otp(@user).deliver_now
+#   			render :json =>  {:responseCode => 200,:responseMessage =>"Your OTP successfully send to your account email, Please verify your account.",:user => @user}
+# 		else
+#   			render :json =>  {:responseCode => 500,:responseMessage => @user.errors.full_messages.first }
+# 		end	
+#   	end
 
-  	def update_user
-  		if @user.update_attributes(user_params)
-  			render :json =>  {:responseCode => 200,:responseMessage =>"Your account has been updated successfully.",:user => @user}	
-  		else
-		    render :json =>  {:responseCode => 500,:responseMessage => @user.errors.full_messages.first}
-		end	
-  	end
+#   	def update_user
+#   		if @user.update_attributes(user_params)
+#   			render :json =>  {:responseCode => 200,:responseMessage =>"Your account has been updated successfully.",:user => @user}	
+#   		else
+# 		    render :json =>  {:responseCode => 500,:responseMessage => @user.errors.full_messages.first}
+# 		end	
+#   	end
 
-  	def sign_in
-	    if @user = User.find_by_user_name(params[:user][:user_name]) || @user = User.find_by_email(params[:user][:email].downcase)
-	        if (@user && @user.valid_password?(params[:user][:password]))
-		        @device = Device.find_or_create_by(device_id: params[:device_id], device_type: params[:device_type]).update(user_id: @user.id)
-	        	render :json =>  {:responseCode => 200,:responseMessage =>"User login successfully.",:user => @user}
-	      	else
-	        	render :json =>  {:responseCode => 500,:responseMessage =>"Invalid Email or Password, Please try again.",:user => @user.errors.full_messages.first}
-	      	end
-	    else
-	      render :json =>  {:responseCode => 500,:responseMessage => "Invalid Email or Username, Please enter a valid email or Username."}
-	    end
-  	end
+#   	def sign_in
+# 	    if @user = User.find_by_user_name(params[:user][:user_name]) || @user = User.find_by_email(params[:user][:email].downcase)
+# 	        if (@user && @user.valid_password?(params[:user][:password]))
+# 		        @device = Device.find_or_create_by(device_id: params[:device_id], device_type: params[:device_type]).update(user_id: @user.id)
+# 	        	render :json =>  {:responseCode => 200,:responseMessage =>"User login successfully.",:user => @user}
+# 	      	else
+# 	        	render :json =>  {:responseCode => 500,:responseMessage =>"Invalid Email or Password, Please try again.",:user => @user.errors.full_messages.first}
+# 	      	end
+# 	    else
+# 	      render :json =>  {:responseCode => 500,:responseMessage => "Invalid Email or Username, Please enter a valid email or Username."}
+# 	    end
+#   	end
 
-  	def social_login
-		@user = User.find_by(email: params[:user][:email].downcase, provider: params[:user][:provider], u_id: params[:user][:u_id])
-		if @user
-		    @user.update_attributes(first_name: params[:first_name],last_name: params[:last_name])
-		    @device = Device.find_or_create_by(device_id: params[:device_id], device_type: params[:device_type], user_id: @user.id)
-		    render :json => {responseCode: 200,responseMessage: "User login successfully.",user: @user }
-		else
-			@otp = OtpInfo.find_or_create_by(email: params[:user][:email])
-	    	@otp.otp =  rand(1000..9999)
-	    	if @otp.save
-		    	UserMailer.send_otp(@otp).deliver_now
-			    render :json =>  {:responseCode => 200,:responseMessage =>"Your OTP successfully send to your account email, Please verify your OTP to create your account.",:user => @otp }
-   			end
-		end
-	end
+#   	def social_login
+# 		@user = User.find_by(email: params[:user][:email].downcase, provider: params[:user][:provider], u_id: params[:user][:u_id])
+# 		if @user
+# 		    @user.update_attributes(first_name: params[:first_name],last_name: params[:last_name])
+# 		    @device = Device.find_or_create_by(device_id: params[:device_id], device_type: params[:device_type], user_id: @user.id)
+# 		    render :json => {responseCode: 200,responseMessage: "User login successfully.",user: @user }
+# 		else
+# 			@otp = OtpInfo.find_or_create_by(email: params[:user][:email])
+# 	    	@otp.otp =  rand(1000..9999)
+# 	    	if @otp.save
+# 		    	UserMailer.send_otp(@otp).deliver_now
+# 			    render :json =>  {:responseCode => 200,:responseMessage =>"Your OTP successfully send to your account email, Please verify your OTP to create your account.",:user => @otp }
+#    			end
+# 		end
+# 	end
 
 
-  	def sign_out
-	    @device = Device.find_or_create_by(device_id: params[:device_id], device_type: params[:device_type])
-	    if @user
-	      @device.update(device_id: "")
-	      render :json => {:responseCode => 200,:responseMessage => "You've successfully logged out."}
-	    end     
-	end
+#   	def sign_out
+# 	    @device = Device.find_or_create_by(device_id: params[:device_id], device_type: params[:device_type])
+# 	    if @user
+# 	      @device.update(device_id: "")
+# 	      render :json => {:responseCode => 200,:responseMessage => "You've successfully logged out."}
+# 	    end     
+# 	end
 
-	def forget_password
-		@user = User.find_by(:email => params[:email].downcase)
-		if @user
-			User.send_password_reset(@user)
-			render :json => {:responseCode => 200,:responseMessage => "Your new password send to your email, please find and reset password."}
-		else
-			render :json => {:responseCode => 500,:responseMessage => "Your email doesn't exist, please verify your email."}
-		end	 
-	end
+# 	def forget_password
+# 		@user = User.find_by(:email => params[:email].downcase)
+# 		if @user
+# 			User.send_password_reset(@user)
+# 			render :json => {:responseCode => 200,:responseMessage => "Your new password send to your email, please find and reset password."}
+# 		else
+# 			render :json => {:responseCode => 500,:responseMessage => "Your email doesn't exist, please verify your email."}
+# 		end	 
+# 	end
 
-	def change_password
-		if @user.valid_password?(params[:user][:old_password])
-	    	if @user.update_attributes(password: params[:user][:new_password], password_confirmation: params[:user][:new_password_confirmation])
-			  	render :json => {:responseCode => 200,:responseMessage => "Your Password has been reset successfully"}
-	    	else
-	      		render :json => {:responseCode => 500,:responseMessage => "Your password can't be reset, please try again."}
-	    	end
-	  	else
-	  		render :json => {:responseCode => 500,:responseMessage => "Your old password has not been matched."	}
-	  	end  
-	end
+# 	def change_password
+# 		if @user.valid_password?(params[:user][:old_password])
+# 	    	if @user.update_attributes(password: params[:user][:new_password], password_confirmation: params[:user][:new_password_confirmation])
+# 			  	render :json => {:responseCode => 200,:responseMessage => "Your Password has been reset successfully"}
+# 	    	else
+# 	      		render :json => {:responseCode => 500,:responseMessage => "Your password can't be reset, please try again."}
+# 	    	end
+# 	  	else
+# 	  		render :json => {:responseCode => 500,:responseMessage => "Your old password has not been matched."	}
+# 	  	end  
+# 	end
 
-  	private
-	def user_params
-		params[:user][:image] = User.image_data(params[:user][:image].to_s.gsub("\\r\\n","")) if params[:user][:image]
-	    params.require(:user).permit(:first_name, :last_name, :user_name, :email, :password, :password_confirmation, :otp, :tc_accept, :image, :role, :address, :provider, :u_id)
-	end   
-	def find_user
-		if params[:user][:user_id]
-			@user = User.find_by_id(params[:user][:user_id])
-		    unless @user
-		     render :json =>  {:responseCode => 500,:responseMessage => "Oops! User not found."}
-		    end
-		else
-			render :json =>  {:responseCode => 500,:responseMessage => "Sorry! You are not an authenticated user."}
-	    end
-  	end
-end
+#   	private
+# 	def user_params
+# 		params[:user][:image] = User.image_data(params[:user][:image].to_s.gsub("\\r\\n","")) if params[:user][:image]
+# 	    params.require(:user).permit(:first_name, :last_name, :user_name, :email, :password, :password_confirmation, :otp, :tc_accept, :image, :role, :address, :provider, :u_id)
+# 	end   
+# 	def find_user
+# 		if params[:user][:user_id]
+# 			@user = User.find_by_id(params[:user][:user_id])
+# 		    unless @user
+# 		     render :json =>  {:responseCode => 500,:responseMessage => "Oops! User not found."}
+# 		    end
+# 		else
+# 			render :json =>  {:responseCode => 500,:responseMessage => "Sorry! You are not an authenticated user."}
+# 	    end
+#   	end
+# end
