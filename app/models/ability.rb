650d2307b37e04d7674660fd0df812a1806ef902
shashank_tripathi <shashanktripathi1993@yahoo.com>
2016-06-14 12:12:39 +0530

    change in production

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,24 +1,23 @@
 class Ability < ActiveRecord::Base
-	# can :manage, :all
-	# include CanCan::Ability
+	include CanCan::Ability
 
-	# def initialize(admin_user)
-	#     case admin_user.role
-	#       when 'corporate-user'
-	#         can :read, ActiveAdmin::Page, :name => "Dashboard"
-	#         if admin_user.adds.eql?(true) 
-	#         	can :manage, Advertisement 
-	#         end
-	#         can :manage, Trending 
-	#         if admin_user.feeds.eql?(true)
-	# 	        can :manage,Post 
-	# 	        can :manage,Video
-	# 	    end
-	#       when 'super-admin'
-	#         can :manage, :all
-	#         cannot :destroy, AdminUser, id: 1 
-	#     end
-  	# end
+	def initialize(admin_user)
+	    case admin_user.role
+	      when 'corporate-user'
+	        can :read, ActiveAdmin::Page, :name => "Dashboard"
+	        if admin_user.adds.eql?(true) 
+	        	can :manage, Advertisement 
+	        end
+	        can :manage, Trending 
+	        if admin_user.feeds.eql?(true)
+		        can :manage,Post 
+		        can :manage,Video
+		    end
+	      when 'super-admin'
+	        can :manage, :all
+	        cannot :destroy, AdminUser, id: 1 
+	    end
+  	end
 
   	# def initialize(user)
 	  #   case user.role
