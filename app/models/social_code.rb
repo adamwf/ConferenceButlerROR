d6f5aa19a4bc36054bdafeb69f68446219529d24
shashank_tripathi <shashanktripathi1993@yahoo.com>
2016-06-15 13:10:49 +0530

    change in public qrcodes

--- app/models/social_code.rb
+++ app/models/social_code.rb
@@ -11,11 +11,11 @@ class SocialCode < ActiveRecord::Base
 		p "--13---------#{qr.inspect}-------------------------"
 		png = qr.to_img  
 		p "--14---------#{png.inspect}-------------------------"                                           # returns an instance of ChunkyPNG
-		File.open(Rails.root.join("public/qrcodes/#{random_name}.png"), 'wb'){|f| f.write png }
+		File.open(Rails.root.join("public/#{random_name}.png"), 'wb'){|f| f.write png }
 		p "--15---------#{png.inspect}-------------------------"
-		image = Cloudinary::Uploader.upload(Rails.root.join("public/qrcodes/#{random_name}.png"))
+		image = Cloudinary::Uploader.upload(Rails.root.join("public/#{random_name}.png"))
 		p "--16---------#{image.inspect}-------------------------"
-		File.delete("./public/qrcodes/#{random_name}.png")
+		File.delete("./public/#{random_name}.png")
 		p "--17---------#{image.inspect}-------------------------"
 		p image.as_json(only: ["public_id", "url"])
 	end
