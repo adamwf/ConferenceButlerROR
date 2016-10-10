fea96f60606aebf0914d381b02e2404debeb7ae4
Pawan Yadav <pawan.yadav@mobiloittegroup.com>
2016-10-10 15:07:10 +0530

    added design for contact page

--- app/controllers/handle_directory/friends_controller.rb
+++ app/controllers/handle_directory/friends_controller.rb
@@ -2,9 +2,19 @@ class HandleDirectory::FriendsController < HandleDirectory::BaseController
   before_filter :require_handle_user
 
   def index
-    @friends = current_handle_user.friends
+    if params[:search].present?
+      if params[:search][:keyword].present?
+        @search = current_handle_user.friendships.where("keyword LIKE ?", "%#{params[:search][:keyword]}%")
+      else
+        @search = current_handle_user.friendships
+      end
+      @friends = @search.map{|key| key.friend}.sort_by {|friend| friend[params[:search][:order_by]]}.paginate(:page => params[:page], :per_page => 10)
+    else
+      @friends = current_handle_user.friends.paginate(:page => params[:page], :per_page => 10)
+    end
   end
 
   def show
   end
 end
+ User.find(4).approve User.first
