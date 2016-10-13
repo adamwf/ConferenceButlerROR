ee6827fc4c428a76725b30de3366d502a6ea7855
shashank_tripathi <shashank.tripathi@mobiloittegroup.com>
2016-10-13 22:10:20 +0530

    updated code

--- app/helpers/activities_helper.rb
+++ app/helpers/activities_helper.rb
@@ -1,7 +1,7 @@
 module ActivitiesHelper
 
 	def get_tab tab, current_tab
-		p "*****#{tab}*****#{current_tab}"
+		p "*****#{tab}*****#{current_tab}------"
 		if tab.to_i == current_tab.to_i
 			return "active"
 		else
@@ -9,7 +9,7 @@ module ActivitiesHelper
 		end
 	end
 	def get_pane tab, current_tab
-		p "*****#{tab}*****#{current_tab}"
+		p "*****#{tab}*****#{current_tab}-------"
 		if tab.to_i == current_tab.to_i
 			return "active in"
 		else
